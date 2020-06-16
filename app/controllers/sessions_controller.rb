a74ff7f424acfba0e3abbccd5d121ad08cc18772
wcl <wcl19941007@163.com>
2020-06-16 21:54:12 +0800

    add user and session and user settings

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  layout 'base'
+
+  def new
+    redirect_to root_path if logged_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:email]&.downcase)
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = t('error.login')
+      render 'new'
+    end
+  end
+
+  def destroy
+    return unless logged_in?
+
+    session.delete(:user_id)
+    redirect_to new_session_path
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  AVAILABLE_THEME_OPTIONS = %w(dark light auto)
+  DEFAULT_THEME = 'dark'
+
+  before_create :downcase_email
+
+  validates :email presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  has_secure_password
+
+  scoped_field :theme, default: DEFAULT_THEME, available_options: AVAILABLE_THEME_OPTIONS
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+end
